3979b8356ccd1aaebb9375620ec6084264de983d
Kevin Christensen <nivekmai@gmail.com>
2019-10-16 01:20:29 -0700

    Update sensor_command to work with new broadlink.send

--- apps/sensor_command.py
+++ apps/sensor_command.py
@@ -6,7 +6,8 @@ class SensorCommand(hass.Hass):
     def initialize(self):
         self.sensor = self.args.get('sensor', 'binary_sensor.hall_bath_door_handle')
         self.packet = self.args.get('package', 'JgD8AMJjEhISNxMSERMSEhIUERMRExETEhMSEhI3EhMRExISEhMSExETERMSExISEhMRExM2EjcROBE3ExISNxE4EhIUNhE3EjcSNxETEjcSNxITEgAD9MNjERMRNxMSEhMRExEUERMSEhITERQRExE3ExISExETERMSExISEhMRFBETERMSEhI4ETcSNxI3EhMRNxI4ERMROBI3ETgRNxITEjcRNxMSEgAD9cNiERMSNxITERMRExITEhISExETEhMRExI3EhMRExETEhMSEhITERMRFBETEhISExE4ETcTNxE3EhMSNxE4ERMSNxM1EjYUNxETFDUSNxITEQANBQAAAAAAAAAAAAAAAA')
-        self.ir_service = self.args.get('ir_service', 'switch/broadlink_send_packet_192_168_1_151')
+        self.ir_service = self.args.get('ir_service', "broadlink/send")
+        self.ir_host = self.args.get('ir_host', '192.168.1.151')
         self.log("Initializing sensor command: sensor {} packet: {} IR service: {}"
                  .format(self.sensor, self.packet, self.ir_service))
         self.listen_state(self.on_sensor_change, self.sensor)
@@ -16,4 +17,4 @@ class SensorCommand(hass.Hass):
         self.log("INFO: sensor state changed ({})".format(new))
         if (new == "on"):
             self.log("INFO: firing packet")
-            self.call_service(self.ir_service, packet = self.packet)
+            self.call_service(self.ir_service, packet = self.packet, host = self.ir_host)
