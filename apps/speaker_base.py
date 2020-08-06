24880431b7927b6896bb5c49c193d57173f06d3e
Kevin Christensen <nivekmai@gmail.com>
2020-08-06 06:35:11 -0700

    Default speaker and tv notifications for speaker_base

--- apps/speaker_base.py
+++ apps/speaker_base.py
@@ -3,9 +3,10 @@ import appdaemon.plugins.hass.hassapi as hass
 
 class SpeakerBase(hass.Hass):
     def initialize(self):
-        self.speakers = self.split_device_list(self.args.get("speakers", ""))
+        self.speakers = self.split_device_list(self.args.get("speakers", "media_player.couch_speaker"))
 
     def say_on_speakers(self, text):
         self.log(text)
         for speaker in self.speakers: 
             self.call_service('tts/google_say', entity_id=speaker, message=text)
+            self.call_service('notify/living_room', message=text)
