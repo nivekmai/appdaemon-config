a3567d2b8f580d707fb2440c65e5d8106b9e8aad
Kevin Christensen <nivekmai@gmail.com>
2021-06-06 09:56:57 -0700

    [Door Light] Fix lock check
    Summary: At some point, it seems that the unlocked state of a lock changed from
    "on" to "unlocked". Updating the script to be able to understand that.
    
    Test Plan: Locked/unlocked front door, observed that light reacted
    properly.

--- apps/doorlight.py
+++ apps/doorlight.py
@@ -20,14 +20,14 @@ class DoorLight(hass.Hass):
         if self.timer_handle:
             self.cancel_timer(self.timer_handle)
         if self.use_sun and self.sun_down() or not self.use_sun:
-            if new == "on":
-                self.log("INFO: door off (open) and sun_down")
+            if new == "unlocked" or new == "on":
+                self.log("INFO: door off (unlocked) and sun_down")
                 self.turn_on(self.light)
             else:
-                self.log("INFO: door on (closed) and sun_down")
+                self.log("INFO: door on (locked) and sun_down")
                 self.timer_handle = self.run_in(self.turn_off_light, self.timeout)
 
     def turn_off_light(self, *args):
         self.cancel_timer(self.timer_handle)
         self.turn_off(self.light)
-        self.log("INFO: runing off light {}".format(self.light))
+        self.log("INFO: turning off light {}".format(self.light))
