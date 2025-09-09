64b8abbcf207dfadc0a448fac4ddd9467f2207f0
Kevin Christensen <nivekmai@gmail.com>
2025-09-09 10:17:12 -0700

    Update two_way_sync with new directories/hardcoded ha ip

--- two_way_sync_from_pc.sh
+++ two_way_sync_from_pc.sh
@@ -3,9 +3,9 @@
 rsync -auv \
 	--exclude '__pycache__' \
 	--exclude '.git' \
-	./ pi:/home/homeassistant/appdaemon
+	./ hassio@192.168.1.2:/root/addon_configs/a0d7b954_appdaemon/
 
 rsync -auv \
 	--exclude '__pycache__' \
 	--exclude '.git' \
-	pi:/home/homeassistant/appdaemon/ ./
+	hassio@192.168.1.2:/root/addon_configs/a0d7b954_appdaemon/ ./
