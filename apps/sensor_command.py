99c6933ad542a3a756c346486e95a1d0ca070f07
Kevin Christensen <nivekmai@gmail.com>
2025-09-09 09:46:30 -0700

    fix sensor_command to have b64 prefix and use command

--- apps/sensor_command.py
+++ apps/sensor_command.py
@@ -5,10 +5,10 @@ class SensorCommand(hass.Hass):
     """Fires off an IR command when a sensor is tripped"""
 
     def initialize(self):
-        self.sensor = self.args.get("sensor", "binary_sensor.hall_bath_door_handle")
+        self.sensor = self.args.get("sensor", "binary_sensor.ecolink_4655bc0_r_iaszone")
         self.packet = self.args.get(
             "package",
-            "JgD8AMJjEhISNxMSERMSEhIUERMRExETEhMSEhI3EhMRExISEhMSExETERMSExISEhMRExM2EjcROBE3ExISNxE4EhIUNhE3EjcSNxETEjcSNxITEgAD9MNjERMRNxMSEhMRExEUERMSEhITERQRExE3ExISExETERMSExISEhMRFBETERMSEhI4ETcSNxI3EhMRNxI4ERMROBI3ETgRNxITEjcRNxMSEgAD9cNiERMSNxITERMRExITEhISExETEhMRExI3EhMRExETEhMSEhITERMRFBETEhISExE4ETcTNxE3EhMSNxE4ERMSNxM1EjYUNxETFDUSNxITEQANBQAAAAAAAAAAAAAAAA",
+            "b64:JgD8AMJjEhISNxMSERMSEhIUERMRExETEhMSEhI3EhMRExISEhMSExETERMSExISEhMRExM2EjcROBE3ExISNxE4EhIUNhE3EjcSNxETEjcSNxITEgAD9MNjERMRNxMSEhMRExEUERMSEhITERQRExE3ExISExETERMSExISEhMRFBETERMSEhI4ETcSNxI3EhMRNxI4ERMROBI3ETgRNxITEjcRNxMSEgAD9cNiERMSNxITERMRExITEhISExETEhMRExI3EhMRExETEhMSEhITERMRFBETEhISExE4ETcTNxE3EhMSNxE4ERMSNxM1EjYUNxETFDUSNxITEQANBQAAAAAAAAAAAAAAAA",
         )
         self.ir_service = self.args.get("ir_service", "broadlink/send")
         self.ir_host = self.args.get("ir_host", "192.168.1.151")
@@ -23,4 +23,4 @@ class SensorCommand(hass.Hass):
         self.log("INFO: sensor state changed ({})".format(new))
         if new == "on":
             self.log("INFO: firing packet")
-            self.call_service(self.ir_service, packet=self.packet, host=self.ir_host)
+            self.call_service(self.ir_service, command=self.packet, host=self.ir_host)
