ac35e9e1a9642cda82abab9154478abd81fab518
Kevin Christensen <nivekmai@gmail.com>
2025-09-09 09:01:17 -0700

    wip testing carnotify fixes

--- apps/car_notify.py
+++ apps/car_notify.py
@@ -1,9 +1,8 @@
 import speaker_base as speaker_base
 
+SpeakerBase = speaker_base.SpeakerBase
 
 class CarNotify(SpeakerBase):
     def initialize(self):
         super().initialize()
-
-
-# self.car = self.args.get('')
+        self.say_on_speakers("hello")
